dbf851548db32ac5451cee5671219570f35c545e
Bresnow <45741877+bresnow@users.noreply.github.com>
2020-12-20 18:25:01 -0500

    Update contact-form-one.js

--- components/contact/contact-form-one.js
+++ components/contact/contact-form-one.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import BlockTitle from "../block-title";
+import emailjs from "emailjs-com";
+import { useForm } from "react-hook-form";
 
 const ContactFormOne = () => {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data,e) => {
+    console.log(data);
+
+    alert(`Thank You. Your message has been recieved`);
+    e.preventDefault();
+    emailjs
+    .sendForm(
+      "service_x81uten",
+      "dbcontact_page",
+      e.target,
+      "user_qmKiHeFhCxOB8fG5R38r3"
+    )
+    .then((result) => {
+      console.log(result.text);
+      (error) => {
+        console.log(error.text);
+      };
+    })
+    console.log(watch("name"));
+
+  };
+
   return (
     <section className="contact-page pt-120 pb-80">
       <Container>
@@ -13,9 +38,7 @@ const ContactFormOne = () => {
                 title={`Feel free to write us \n a message.`}
                 tagLine="Contact Us"
               />
-              <p className="block-text mb-30 pr-10">
-                {" "}
-              </p>
+              <p className="block-text mb-30 pr-10"> </p>
               <div className="footer-social black-hover">
                 <a href="#" aria-label="twitter">
                   <i className="fab fa-twitter"></i>
@@ -33,35 +56,66 @@ const ContactFormOne = () => {
             </div>
           </Col>
           <Col lg={7}>
-            <form className="contact-form-validated contact-page__form form-one mb-40">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="contact-form-validated contact-page__form form-one mb-40"
+            >
               <div className="form-group">
                 <div className="form-control">
                   <label htmlFor="name" className="sr-only">
                     Name
                   </label>
                   <input
+                    ref={register({ required: true })}
                     type="text"
                     name="name"
                     id="name"
                     placeholder="Your Name"
                   />
+                  {errors.name && (
+                    <p
+                      style={{
+                        color: "red",
+                        alignContent: "center",
+                        fontSize: 16,
+                      }}
+                    >
+                      This field is required
+                    </p>
+                  )}
                 </div>
                 <div className="form-control">
                   <label htmlFor="email" className="sr-only">
-                    email
+                    Email
                   </label>
                   <input
+                    ref={register({
+                      required: true,
+                      pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    })}
                     type="text"
                     name="email"
                     id="email"
                     placeholder="Email Address"
                   />
+                  {errors.email && (
+                    <p
+                      style={{
+                        color: "red",
+                        alignContent: "center",
+                        fontSize: 16,
+                      }}
+                    >
+                      This field is required and must be a valid email address.
+                    </p>
+                  )}
                 </div>
                 <div className="form-control">
                   <label htmlFor="phone" className="sr-only">
                     phone
                   </label>
                   <input
+                    ref={register}
                     type="text"
                     name="phone"
                     id="phone"
@@ -73,6 +127,7 @@ const ContactFormOne = () => {
                     subject
                   </label>
                   <input
+                    ref={register}
                     type="text"
                     name="subject"
                     id="subject"
@@ -84,19 +139,19 @@ const ContactFormOne = () => {
                     message
                   </label>
                   <textarea
+                    ref={register}
                     name="message"
                     placeholder="Write a Message"
                     id="message"
                   ></textarea>
                 </div>
                 <div className="form-control form-control-full">
-                  <button type="submit" className="thm-btn ">
+                  <button  type="submit" className="thm-btn ">
                     Submit Message
                   </button>
                 </div>
               </div>
             </form>
-            <div className="result"></div>
           </Col>
         </Row>
       </Container>
